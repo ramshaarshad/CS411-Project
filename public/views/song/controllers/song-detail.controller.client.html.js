087f6858017c7e2b422f719301ff2034cbf398df
ramshaarshad <32443688+ramshaarshad@users.noreply.github.com>
2019-04-24 11:41:33 -0400

    Create song-detail.controller.client.html.js

---
+++ public/views/song/controllers/song-detail.controller.client.html.js
@@ -0,0 +1,44 @@
+(function() {
+    angular.module('SpotifyParty')
+        .controller('SongDetailController', SongDetailController);
+
+    function SongDetailController(SongService, PartyService, $window, $routeParams, $location) {
+        const model = this;
+
+        model.goToSongList = goToSongList;
+        model.goToParty = goToParty;
+        model.goBack = goBack;
+
+        const trackId = $routeParams['trackId'];
+        const partyId = $routeParams['partyId'];
+        model.pageTitle = 'Song Details';
+        function init() {
+            SongService.getSongById(trackId)
+                .then(function(songDetails){
+                    const songLenthMin = songDetails.duration / 1000 / 60;
+                    const songLenthSec = (songLenthMin - (Math.floor(songLenthMin))) * 60;
+                    songDetails.duration = (Math.floor(songLenthMin)) + ':' + (_.padStart((Math.round(songLenthSec)), 2, '0'));
+                    model.song = songDetails;
+                });
+
+            PartyService.getPartiesWithSong(trackId)
+                .then((parties) => {
+                   model.parties = parties;
+                });
+        }
+        init();
+
+        function goToSongList() {
+            $location.path('/party/' + partyId + '/add-song');
+        }
+
+        function goBack() {
+            $window.history.back();
+        }
+
+        function goToParty(party) {
+            $location.path('/party/' + party._id);
+        }
+
+    }
+})();
