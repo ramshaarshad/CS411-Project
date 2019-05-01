95abe1fd2369caab84d51c54ad71f65166b7c86f
ramshaarshad <32443688+ramshaarshad@users.noreply.github.com>
2019-05-01 19:45:40 -0400

    Abstracting client id, client secret, callback URL

--- app/config/config.js
+++ app/config/config.js
@@ -5,6 +5,6 @@ const spotifyClientSecret = process.env.SPOTIFY_CLIENT_SECRET;
 const spotifyCallbackURL = process.env.SPOTIFY_CALLBACK_URL;
 
 module.exports.port = port;
-module.exports.spotifyClientId = '2a9b35de21454938b66d40315dc0453b';
-module.exports.spotifyClientSecret = '22eed44f4452495bbb0025a138beac81';
-module.exports.spotifyCallackURL = 'http://127.0.0.1:8081/callback/';
+module.exports.spotifyClientId = spotifyClientId;
+module.exports.spotifyClientSecret = spotifyClientSecret;
+module.exports.spotifyCallbackURL = spotifyCallbackURL;
